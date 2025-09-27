0a4942d5ef9ee9e372e816824c683219329bed43
Nobahu <sasha.lyamburtsev@gmail.com>
2025-09-27 15:09:31 +0700

    Multi-channel server with request processing

---
+++ PoissonGen.cpp
@@ -0,0 +1,20 @@
+﻿#include "SimpleServer.h"
+
+using namespace std;
+
+const int SEED = 245;
+const double lambda = 3.0;
+
+int main() {
+
+	setlocale(LC_ALL, "RU");
+
+	double mu = 100.0;
+	milliseconds workTime = 1000ms;
+
+	MultichannelServer server(20, lambda, mu, SEED, workTime);
+	server.run();
+	server.printStats();
+}
+
+
